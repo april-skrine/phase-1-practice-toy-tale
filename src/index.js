20dd925b609abd998ad6e0b7c3bd26735c425d52
April Skrine <april.skrine@gmail.com>
2022-01-19 16:31:01 -0600

    For Dalton

--- src/index.js
+++ src/index.js
@@ -13,3 +13,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyUrl = 'http://localhost:3000/toys'
+
+function fetchToy() {
+  fetch(toyUrl)
+  .then(resp => resp.json())
+  .then( data => data.forEach(renderToy))
+}
+fetchToy()
+
+function renderToy(someObj) {
+  const toyCard = document.createElement('div')
+  toyCard.className = "card"
+  const toyHeader = document.createElement('h2')
+  toyHeader.innerText = someObj.name
+  const toyImg = document.createElement('img')
+  toyImg.src = `${someObj.image}`
+  toyImg.className = "toy-avatar"
+  const toyLikes = document.createElement('p')
+  toyLikes.innerText = `${someObj.likes} likes`
+  const toyButton = document.createElement('button')
+  toyButton.className = "like-btn"
+  toyButton.id = someObj.id
+  toyButton.innerText = "Like <3"
+  toyButton.addEventListener('click', () => {
+    someObj.likes++
+    toyLikes.innerText = `${someObj.likes} likes` })
+  toyCard.append(toyHeader, toyImg, toyLikes, toyButton)
+  document.getElementById("toy-collection").append(toyCard)
+}
